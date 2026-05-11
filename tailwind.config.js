ad987d501f529994dfde6145afab143201bd7a78
veleskasofiia <veleskasofija@gmail.com>
2026-05-11 16:24:24 +0300

    Initial commit with Tailwind setup and layout fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: "class",
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
