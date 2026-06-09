29082ad3c7defdb2931fb19aa11432577496bb37
veleskasofiia <veleskasofija@gmail.com>
2026-06-09 10:59:14 +0200

    Add landing page and login navigation tests

--- tests/landing.spec.ts
+++ tests/landing.spec.ts
@@ -14,3 +14,20 @@ test('Page loads and has the title FlowBoard', async ({ page }) => {
     await expect(page).toHaveTitle(/FlowBoard/);
 
 });
+
+//Test_3_button_""Get_started_free"_is_visible
+
+test('Get started free button is visible', async ({ page }) => {
+    await page.goto('/');
+    await expect(page.getByRole('link', {name: 'Get Started Free' })).toBeVisible();
+
+}); 
+
+//Test_4_LogIn
+
+test('LogIn to app', async ({ page }) => {
+    await page.goto('/');
+    await page.getByRole ('link', {name:'Sign In'}).first().click();
+    await expect(page).toHaveURL('/auth/login'); 
+
+});
